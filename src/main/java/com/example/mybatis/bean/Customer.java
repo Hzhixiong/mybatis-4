5a068e52da6060c79ab2fb467dfcc6c8a52f98ac
hzx <1285180855@qq.com>
2019-12-26 15:53:19 +0800

    初始化项目

---
+++ src/main/java/com/example/mybatis/bean/Customer.java
@@ -0,0 +1,51 @@
+package com.example.mybatis.bean;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private Integer id;
+
+    private Double money;
+
+    private String name;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", money=").append(money);
+        sb.append(", name=").append(name);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
